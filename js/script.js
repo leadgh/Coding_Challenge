635d3e94de2b87ef86dbe4555213ebd3503c2524
leadgh <leadgh@users.noreply.github.com>
2019-12-13 12:11:53 +0100

    partage

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Exercice 2.1
+function Division() {
+	var nb_1 = document.getElementById("ex1_input1").value;
+	var nb_2 = document.getElementById("ex1_input2").value;
+	var result1 = nb_1 / nb_2;
+    document.getElementById('result-exo-1').innerHTML = result1;
+}
